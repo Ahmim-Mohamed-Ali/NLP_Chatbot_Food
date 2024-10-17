2bc57a81bd08ce4b6afd9f3b28c08038d6224a79
Youcef782 <youcefahmim78@gmail.com>
2024-10-17 12:48:36 +0200

    Initial commit of FoodChatBot project

---
+++ backend/db_helper.py
@@ -0,0 +1,128 @@
+import mysql.connector
+
+
+# Connexion à la base de données
+cnx = mysql.connector.connect(
+    host="localhost",  # Remplace par l'hôte de ta base de données
+    user="root",  # Remplace par ton utilisateur MySQL
+    password="root",  # Remplace par ton mot de passe MySQL
+    database="pandeyji_eatery"  # Nom de ta base de données
+)
+
+
+def insert_order_item(food_item, quantity, order_id):
+    try:
+        cursor = cnx.cursor()
+
+        # Calling the stored procedure
+        cursor.callproc('insert_order_item', (food_item, quantity, order_id))
+
+        # Committing the changes
+        cnx.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+        print("Order item inserted successfully!")
+
+        return 1
+
+    except mysql.connector.Error as err:
+        print(f"Error inserting order item: {err}")
+
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
+
+# Function to insert a record into the order_tracking table
+def insert_order_tracking(order_id, status):
+    cursor = cnx.cursor()
+
+    # Inserting the record into the order_tracking table
+    insert_query = "INSERT INTO order_tracking (order_id, status) VALUES (%s, %s)"
+    cursor.execute(insert_query, (order_id, status))
+
+    # Committing the changes
+    cnx.commit()
+
+    # Closing the cursor
+    cursor.close()
+
+def get_total_order_price(order_id):
+    cursor = cnx.cursor()
+
+    # Executing the SQL query to get the total order price
+    query = f"SELECT get_total_order_price({order_id})"
+    cursor.execute(query)
+
+    # Fetching the result
+    result = cursor.fetchone()[0]
+
+    # Closing the cursor
+    cursor.close()
+
+    return result
+
+# Function to get the next available order_id
+def get_next_order_id():
+    cursor = cnx.cursor()
+
+    # Executing the SQL query to get the next available order_id
+    query = "SELECT MAX(order_id) FROM orders"
+    cursor.execute(query)
+
+    # Fetching the result
+    result = cursor.fetchone()[0]
+
+    # Closing the cursor
+    cursor.close()
+
+    # Returning the next available order_id
+    if result is None:
+        return 1
+    else:
+        return result + 1
+
+
+def get_order_status(order_number:int):
+
+    # Création d'un curseur pour exécuter des requêtes
+    cursor = cnx.cursor()
+
+    # ID de la commande que tu veux vérifier
+    order_id = order_number # Remplace par ton ID récupéré
+
+    # Requête pour récupérer l'état de la commande
+    query = """
+           SELECT status 
+           FROM order_tracking 
+           WHERE order_id = %s
+       """
+
+    # Exécution de la requête
+    cursor.execute(query, (order_id,))
+
+    # Récupération du résultat
+    result = cursor.fetchone()
+
+    # Fermeture du curseur
+    cursor.close()
+    # Vérification de l'état de la commande
+    if result:
+        return result[0]
+
+    else:
+        return None
+
+
+
+
+
---
+++ backend/main.py
@@ -0,0 +1,153 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import generic_helper
+import db_helper
+app = FastAPI(debug=True)
+
+inprogress_orders={}
+@app.get("/")  # Endpoint pour tester GET
+async def get_root():
+    return {"message": "This is a GET response"}
+
+
+
+@app.post("/")  # Utilisez POST car Dialogflow envoie des données via POST
+async def root(request: Request):
+    # Retrieve the JSON data from the request
+    payload = await request.json()
+
+
+
+    # Extract the necessary information from the payload
+    # based on the structure of the WebhookRequest from Dialogflow
+    intent = payload['queryResult']['intent']['displayName']
+    parameters = payload['queryResult']['parameters']
+    output_contexts = payload['queryResult']['outputContexts']
+
+    session_id= generic_helper.extract_session_id(output_contexts[0]["name"])
+
+    intent_handler_dict = {
+        'order.add context:ongoing-order': add_to_order,
+        'order.remove - context:ongoing-order': remove_from_order,
+        'order.complete - context: ongoing-order': complete_order,
+        'track.order-context:ongoing-tracking': track_order
+    }
+
+    return intent_handler_dict[intent](parameters,session_id)
+
+
+
+
+def track_order(parameters:dict,session_id:str):
+    id = parameters["number"]
+    status = db_helper.get_order_status(id)
+    print(f"Order Status: {status}")
+
+    if status:
+        fulfillment_text=f" Votre Commande Numéroe {id} est actuellement : {status}"
+    else:
+        fulfillment_text = f" Votre Commande Numéroe {id} n'a pas été trouvée"
+
+    print(f"Fulfillment Text: {fulfillment_text}")
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+        # "outputContexts": output_contexts  # Echoing back the output contexts if needed
+    })
+
+def add_to_order(paramaters:dict,session_id:str):
+    food_items = paramaters["food-items"]
+    quantities = paramaters["number"]
+
+    if len(food_items) != len(quantities):
+        fulfillment_text = "Sorry I didn't understand. Can you please specify food items and quantities clearly?"
+    else:
+        new_food_dict=dict(zip(food_items,quantities))
+        if session_id in inprogress_orders:
+            inprogress_orders[session_id].update(new_food_dict)
+        else:
+            inprogress_orders[session_id]=new_food_dict
+        result= generic_helper.get_str_from_food_dict(inprogress_orders[session_id])
+        fulfillment_text = f"So Far you have this order {result}. Do You need Anything Else ?"
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+        # "outputContexts": output_contexts  # Echoing back the output contexts if needed
+    })
+
+
+def remove_from_order(parameters:dict,session_id:str):
+    if session_id not in inprogress_orders:
+        return JSONResponse(content={
+            "fulfillmentText": "I'm having a trouble finding your order. Sorry! Can you place a new order please?"
+        })
+
+    food_items = parameters["food-items"]
+    current_order = inprogress_orders[session_id]
+
+    removed_items = []
+    no_such_items = []
+
+    for item in food_items:
+        if item not in current_order:
+            no_such_items.append(item)
+        else:
+            removed_items.append(item)
+            del current_order[item]
+
+    fulfillment_text=""
+    if len(removed_items) > 0:
+        fulfillment_text = f'Removed {",".join(removed_items)} from your order!'
+
+    if len(no_such_items) > 0:
+        fulfillment_text = f' Your current order does not have {",".join(no_such_items)}'
+
+    if len(current_order.keys()) == 0:
+        fulfillment_text += " Your order is empty!"
+    else:
+        order_str = generic_helper.get_str_from_food_dict(current_order)
+        fulfillment_text += f" Here is what is left in your order: {order_str}"
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+    })
+
+
+def complete_order(parameters:dict,session_id:str):
+    if session_id not in inprogress_orders:
+        fulfillment_text="I'm having a trouble finding your order. Sorry Can you place an other"
+    else:
+        order=inprogress_orders[session_id]
+        order_id=save_to_db(order)
+        print(order_id)
+        if order_id==-1:
+            fulfillment_text = "Sorry I Couldn't place your order . PlEASE TRY AGAIN"
+        else:
+            order_total= db_helper.get_total_order_price(order_id)
+            fulfillment_text = f"Awesome. We have placed your order. " \
+                               f"Here is your order id # {order_id}. " \
+                               f"Your order total is {order_total} which you can pay at the time of delivery!"
+            del inprogress_orders[session_id]
+
+    return JSONResponse(content={
+            "fulfillmentText": fulfillment_text
+    })
+
+
+def save_to_db(order: dict):
+    next_order_id = db_helper.get_next_order_id()
+
+    # Insert individual items along with quantity in orders table
+    for food_item, quantity in order.items():
+        rcode = db_helper.insert_order_item(
+            food_item,
+            quantity,
+            next_order_id
+        )
+
+        if rcode == -1:
+            return -1
+
+    # Now insert order tracking status
+    db_helper.insert_order_tracking(next_order_id, "in progress")
+
+    return next_order_id
